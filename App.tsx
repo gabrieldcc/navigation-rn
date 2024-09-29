178ca584e7e54ceea07549515375426bc190d8ad
Gabriel Castro <gabrieldcc@gmail.com>
2024-09-29 11:20:38 -0300

    layout and comments

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler'; // Necessário para o drawer
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native'; //Este componente é o contêiner principal para gerenciar o estado da navegação em seu aplicativo. Ele deve envolver todos os navegadores que você cria.
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -10,13 +10,23 @@ import DetailsScreen from './src/screens/DetailsScreen';
 import ProfileScreen from './src/screens/Profile';
 import SettingsScreen from './src/screens/Settings';
 
-console.log(`Settings Screen: ${SettingsScreen}`)
-
 // Criação de navegadores
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Tabs" component={TabsNavigator} options={{ title: '' }} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} options={{ title: 'Settings' }}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+//Define um navegador de pilha que contém duas telas: HomeScreen e DetailsScreen.
 function HomeStack() {
   return (
     <Stack.Navigator>
@@ -26,22 +36,14 @@ function HomeStack() {
   );
 }
 
+//Define um navegador de abas com duas abas: uma que apresenta o HomeStack (que inclui as telas de Home e Details) e outra que apresenta ProfileScreen.
 function TabsNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="HomeStack" component={HomeStack} options={{ title: 'Home' }} />
+      <Tab.Screen name="HomeStack" component={HomeStack} options={{ title: 'HomeStack', headerShown: false }} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Tabs" component={TabsNavigator} options={{ title: 'Home & Profile' }} />
-        <Drawer.Screen name="Settings" component={SettingsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
+
